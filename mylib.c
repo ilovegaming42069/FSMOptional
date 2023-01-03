ff351ef2d2aac081365686947b559dc8075204ae
ilovegaming42069 <114389460+ilovegaming42069@users.noreply.github.com>
2023-01-04 02:11:01 +0700

    Delete mylib.c

--- mylib.c
+++
@@ -1,6 +0,0 @@
-void states(int s0, int s1, int P, int D, int* S0, int* S1, int* M, int* S) {
-    *S0 = s0 & (!s1) | (!s0) & s1 & D | s0 & (!P) & (!D);
-    *S1 = s1 & (!D) | (!s0) & (!s1) & P | s0 & (!s1) & (!P);
-    *M = s1 & (!D) | (!s0) & (!s1) & P | s0 & (!s1) & (!P);
-    *S = (!s0) & s1 | P & (!D) | (!s0) & P | (!s1) & P;
-}
